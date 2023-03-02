e9b1e079f0505ae03f28c8504c5c77846ac56554
Hannes Ill <54182904+hannesill@users.noreply.github.com>
2023-03-02 23:54:13 +0000

    Finish 2_2_introToLists.hs

--- 2_2_introToLists.hs
+++ 2_2_introToLists.hs
@@ -1,11 +1,7 @@
 -- Intro to lists
-
 appendLists xs ys = xs ++ ys
-
 appendItemToList x xs = x:xs
-
 getItemFromList x xs = xs !! x
-
 trueValue = [1,2,3] == 1:2:3:[]
 
 headOfList = head [1,2,3,4,5] -- =1
@@ -43,8 +39,25 @@ infiniteReps = take 10 (repeat 1) -- same as cycle [1]. Better
 replicateItem = replicate 3 10
 
 -- List comprehension
-firstTenEvenNums = [x*2 | x <- [1..10], x*2 >= 12]
--- ...
+firstTenEvenNumsOver11 = [x*2 | x <- [1..10], x*2 >= 12]
+onlyLargeProducts = [x*y | x <- [1..10], y <-[1..10], x*y > 80]
+length' xs = sum [1 | _ <- xs]
+onlyEvenNumsInSubLists = [[x | x <- xs, even x] | xs <- [[1,2,3,4],[5,8,2],[8,9]]] -- if the comprehensions become too big to comprehend :b you can write them across multiple lines
+
+-- Tuples
+-- Floatunctions for pairs
+firstItem = fst (1,2) -- =1
+secondItem = snd(1,2) -- =2
+enumerateLetters = zip [1..] ['A'..'E'] -- =[(1,A),(2,B),...,(5,E)]
+
+-- Exercise: Which right triangle that has integers for all sides and all sides are equal to or smaller than 10 has a perimeter of 24?
+
+solution = [(a,b,c) | a <- [1..10], b <- [1..10], c <- [1..10], a^2 + b^2 == c^2, a+b+c == 24]
+optimizedSolution = [(a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b],
+                      a^2 + b^2 == c^2, a+b+c == 24] -- if a is always the smallest side
+
+
+
 
 
 
