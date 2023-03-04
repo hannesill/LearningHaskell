bca15b347fdc03f25727dee0c16214046e8d86c9
Hannes Ill <54182904+hannesill@users.noreply.github.com>
2023-03-04 17:26:38 +0000

    Join 2_1 & 2_2 files into 2_introToFunctionsAndLists.hs

--- 2_introToFunctionsAndLists.hs
+++ 2_introToFunctionsAndLists.hs
@@ -1,4 +1,16 @@
--- Intro to lists
+-- 1) Intro to functions
+-- ghci> :l intro to load the intro.hs file
+-- :r to reload after changes
+
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
+
+-- 2) Intro to lists
 appendLists xs ys = xs ++ ys
 appendItemToList x xs = x:xs
 getItemFromList x xs = xs !! x
